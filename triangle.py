7591aee23509d01d3972c5c8da8489ef72adc295
VibishanGovender <vibishan.govender@gmail.com>
2021-08-04 16:48:09 +0200

    last star fix

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
 
 def trianglePrint(Oddnumber):
     for row in range(Oddnumber):
-        for coloumn in range(row + 1):
+        for coloumn in range(row +1): #  +1 will need to print an extra last star in last coloumn for every row
             # deciding which row and coloumn entries will get a * - all perimeter points
 
             if row==coloumn or row==Oddnumber -1 or coloumn==0:
